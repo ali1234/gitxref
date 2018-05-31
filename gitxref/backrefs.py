43f88b06ef71a0708f601582fb46f4e599776cd8
Alistair Buxton <a.j.buxton@gmail.com>
2018-05-31 12:34:13 +0100

    Comment backrefs.py.

--- gitxref/backrefs.py
+++ gitxref/backrefs.py
@@ -9,6 +9,15 @@ from git.repo.fun import name_to_object
 
 class Backrefs(object):
 
+    """
+    Implements a reverse mapping of blobs to the objects which contain them.
+
+    For each object in the repo, add the object's binsha to each of its child
+    object's sets.
+
+    The back references are cached and invalidated if for_each_ref changed.
+    """
+
     def __init__(self, repo):
         self.repo = repo
         self.gitdir = pathlib.Path(repo.git.rev_parse('--absolute-git-dir').strip())
@@ -16,10 +25,17 @@ class Backrefs(object):
 
 
     def check(self):
+        """
+        Check the hash of git for_each_rev. If this changed, the cache is invalid.
+        """
         return hashlib.sha1(self.repo.git.for_each_ref().encode('utf8')).digest()
 
 
     def load(self):
+        """
+        Attempts to load the backrefs from the cache, or regenerate it if there is
+        a problem.
+        """
         hash = self.check()
         check_file = (self.gitdir / 'backrefs.checksum')
         backrefs_file = (self.gitdir / 'backrefs.pickle')
@@ -40,6 +56,9 @@ class Backrefs(object):
 
 
     def generate(self):
+        """
+        Regenerates the backrefs from the repo data.
+        """
         print('Regenerating backrefs database. This may take a few minutes.')
 
         backrefs = defaultdict(set)
@@ -61,10 +80,17 @@ class Backrefs(object):
 
 
     def has_object(self, binsha):
+        """
+        Checks if the given binsha is known in the backrefs database.
+        """
         return binsha in self.backrefs
 
 
     def commits_for_object(self, binsha):
+        """
+        Gets all the commits containing an object. Commits aren't contained by anything,
+        so you found one when there is no further iteration.
+        """
         roots = self.backrefs[binsha]
         for binsha_next in roots:
             if binsha_next in self.backrefs:
